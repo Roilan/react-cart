7fbf107a87aa6d4a22ea70ef047b4963a968c35e
Roilan <roilan14@gmail.com>
2015-04-27 19:17:42 -0400

    added CartComponent and gets data from API. formatted files to use tab space of 2 instead of 4

--- dist/src/js/App.js
+++ dist/src/js/App.js
@@ -1,8 +1,14 @@
 var React = require('react');
 
 var StoreContainer = require('./StoreComponent/StoreContainer');
+var CartContainer = require('./CartComponent/CartContainer');
 
 React.render(
-    <StoreContainer />,
-    document.getElementById('StoreItems')
+  <StoreContainer />,
+  document.getElementById('StoreItems')
+);
+
+React.render(
+  <CartContainer />,
+  document.getElementById('ShoppingCart')
 );
--- dist/src/js/StoreComponent/StoreContainer.js
+++ dist/src/js/StoreComponent/StoreContainer.js
@@ -7,71 +7,70 @@ var api = 'http://104.236.200.153:3000/';
 var apiStore = api + 'api/store';
 
 var StoreContainer = React.createClass({
-    getInitialState: function() {
-        return {
-            items: [],
-            itemAmt: '',
-            itemName: '',
-            itemPrice: ''
-        }
-    },
-    componentDidMount: function() {
-        this.updateStoreItems();
-    },
-    addToStore: function(e) {
-        e.preventDefault();
+  getInitialState: function() {
+    return {
+      items: [],
+      itemAmt: '',
+      itemName: '',
+      itemPrice: ''
+    }
+  },
+  componentDidMount: function() {
+    this.updateStoreItems();
+  },
+  addToStore: function(e) {
+    e.preventDefault();
 
-        if(this.state.itemAmt && this.state.itemName && this.state.itemPrice !== '') {
-            request({
-                url: apiStore,
-                method: 'POST',
-                data: {
-                    price: this.state.itemPrice,
-                    name: this.state.itemName,
-                    amt: this.state.itemAmt
-                },
-                success: function() {
-                    this.updateStoreItems();
-                }.bind(this)
-            });
-        } else {
-            console.log('empty!');
-        }
-    },
-    updateItemState: function(key) {
-        var update = {};
-
-        return function(e) {
-            update[key] = e.target.value;
-            this.setState(update);
-        }.bind(this)
-    },
-    updateStoreItems: function() {
+    if(this.state.itemAmt && this.state.itemName && this.state.itemPrice !== '') {
         request({
             url: apiStore,
-            method: 'GET',
-            success: function(response) {
-                this.setState({
-                    items: response
-                });
+            method: 'POST',
+            data: {
+                price: this.state.itemPrice,
+                name: this.state.itemName,
+                amt: this.state.itemAmt
+            },
+            success: function() {
+                this.updateStoreItems();
             }.bind(this)
         });
-    },
-    render: function() {
-        return (
-            <div>
-                <AddStoreItem
-                    itemName={this.state.itemName}
-                    itemPrice={this.state.itemPrice}
-                    itemAmt={this.state.itemAmt}
-                    updateItemState={this.updateItemState}
-                    addToStore={this.addToStore}
-                />
-
-                <StoreItems items={this.state.items} />
-            </div>
-        )
+    } else {
+      console.log('empty!');
     }
+  },
+  updateItemState: function(key) {
+    var update = {};
+
+    return function(e) {
+      update[key] = e.target.value;
+      this.setState(update);
+    }.bind(this)
+  },
+  updateStoreItems: function() {
+    request({
+      url: apiStore,
+      method: 'GET',
+      success: function(response) {
+        this.setState({items: response});
+      }.bind(this)
+    });
+  },
+  render: function() {
+    return (
+      <div>
+        <AddStoreItem
+          itemName={this.state.itemName}
+          itemPrice={this.state.itemPrice}
+          itemAmt={this.state.itemAmt}
+          updateItemState={this.updateItemState}
+          addToStore={this.addToStore}
+        />
+
+        <StoreItems items={this.state.items} />
+
+      </div>
+    )
+  }
 });
 
 module.exports = StoreContainer;
